2018bd77b9c7bd2ded1d800db93ddcb7fffea0cd
oliverm91 <oliver.mohr.b@gmail.com>
2023-09-27 13:16:25 -0300

    added new class

--- src/OutlookHandler.py
+++ src/OutlookHandler.py
@@ -1,9 +1,43 @@
-from typing import Dict, List
+from typing import Dict, List, Union, Iterable
 import win32com.client
 from datetime import datetime, date
 import os
 
-class MailAttachment:
+class NewMail:
+    def __init__(self, recipient: Union[str, Iterable], copy_recipient: Union[str, Iterable]=None, subject: str="", body: str="", html_body: str="", attachment_path: Union[str, Iterable]=None) -> None:
+        self.recipient = recipient
+        self.copy_recipient = copy_recipient
+        self.subject = subject
+        self.body=body
+        self.html_body=html_body
+        self.attachment_path=attachment_path
+
+    def set_mail_obj(self) -> None:
+        outlook = win32com.client.Dispatch('Outlook.Application')
+        mail = outlook.CreateItem(0)
+        to_recipients = [self.recipient] if not isinstance(self.recipient, Iterable) else self.recipient
+        for rec in to_recipients:
+            r = mail.Recipients.Add(rec)
+            r.Type = 1 # Declare recipient in To
+        if self.copy_recipient is not None:
+            cc_recipients = [self.copy_recipient] if not isinstance(self.copy_recipient, Iterable) else self.copy_recipient
+            for rec in cc_recipients:
+                r = mail.Recipients.Add(rec)
+                r.Type = 2 # Declare recipient in CC
+        if self.subject != "":
+            mail.Subject = self.subject
+        if self.html_body != "":
+            mail.HTMLBody = self.html_body
+        elif self.body != "":
+            mail.Body = self.body
+        
+        if self.attachment_path is not None:
+            attachment_paths = [self.attachment_path] if not isinstance(self.attachment_path, Iterable) else self.attachment_path
+            for ap in attachment_paths:
+                mail.Attachments.Add(ap)        
+        
+
+class ReceivedMailAttachment:
     def __init__(self, pyWin32AttachmentObj) -> None:
         self.pywin32attachment = pyWin32AttachmentObj
         self.filename: str = self.pywin32attachment.filename
@@ -12,7 +46,7 @@ class MailAttachment:
     def save(self, save_dir: str, save_filename: str) -> None:
         self.pywin32attachment.SaveAsFile(os.path.join(save_dir, save_filename))
 
-class Mail:
+class ReceivedMail:
     def __init__(self, pyWin32MailObj) -> None:
         self.pywin32mail = pyWin32MailObj
         self.datetime = datetime(self.pywin32mail.ReceivedTime.year, self.pywin32mail.ReceivedTime.month, self.pywin32mail.ReceivedTime.day,
@@ -23,7 +57,7 @@ class Mail:
         self.body: str = self.pywin32mail.body
         self.html_body: str = self.pywin32mail.htmlbody
         pywin32_attachments_lst = list(self.pywin32mail.attachments)
-        self.attachments: List[MailAttachment] = [MailAttachment(pywin32_att) for pywin32_att in pywin32_attachments_lst if pywin32_att.type == 1]
+        self.attachments: List[ReceivedMailAttachment] = [ReceivedMailAttachment(pywin32_att) for pywin32_att in pywin32_attachments_lst if pywin32_att.type == 1]
         self.has_attachments = len(self.attachments) > 0
     
     def __str__(self) -> str:
@@ -68,7 +102,7 @@ class OutlookHandler:
 
         if filtered_emails.count > 0:
             pywin_mails_lst = list(filtered_emails)
-            mail_lst = [Mail(pywin_mail) for pywin_mail in pywin_mails_lst]
+            mail_lst = [ReceivedMail(pywin_mail) for pywin_mail in pywin_mails_lst]
             folder_mails_dict[folder.name] = mail_lst
 
         # Recursively search in subfolders.
@@ -81,7 +115,7 @@ class OutlookHandler:
                         continue
                 self._search_emails_by_subject_recursive(subfolder, subject_contains, folder_mails_dict, min_date=min_date, max_date=max_date, exact_date=exact_date, folders=folders)
 
-    def get_emails_by_subject(self, subject_contains: str, min_date: date=None, max_date: date=None, exact_date: date=None, folders: List[str]=None, search_in_inbox: bool=False) -> Dict[str, List[Mail]]:
+    def get_emails_by_subject(self, subject_contains: str, min_date: date=None, max_date: date=None, exact_date: date=None, folders: List[str]=None, search_in_inbox: bool=False) -> Dict[str, List[ReceivedMail]]:
         # Start the recursive search from the root folder.
         folder_mails_dict = {}
         if search_in_inbox:
